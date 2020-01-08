4269e9918a90cb968a228a0cd2ed87bda797d56f
Satya <satyap48@gmail.com>
2020-01-08 21:23:25 +0530

    Readded everything

---
+++ GigsNConcerts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigsNConcerts.Startup))]
+namespace GigsNConcerts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
